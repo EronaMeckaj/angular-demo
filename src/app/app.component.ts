6fcc26da2a8dde793bc6ee5a26c5a978c45a2aca
Erona <eronameckaj1@gmail.com>
2024-12-09 13:03:19 +0100

    set up translation with ngx translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-demo';
+export class AppComponent implements OnInit {
+  readonly #translateService = inject(TranslateService);
+
+  ngOnInit(): void {
+    this.setUpLanguage()
+  }
+
+  setUpLanguage() {
+    this.#translateService.addLangs(['en', 'sq']);
+    const lang = localStorage.getItem('language') ?? this.#translateService.getBrowserLang();
+    const selectedLang = lang?.match(/sq|en/) ? lang : 'en';
+    localStorage.setItem('language', selectedLang);
+    this.#translateService.setDefaultLang(selectedLang);
+    this.#translateService.use(selectedLang);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,26 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideHttpClient(withInterceptors([])),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideTranslateService({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })
+  ]
 };
