d5989d975b1b7be5c0664851177ef978d43336ec
Erona <eronameckaj1@gmail.com>
2024-12-28 11:23:59 +0100

    changed the design of error 404 page

--- src/app/error-404/error-404.component.ts
+++ src/app/error-404/error-404.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-error-404',
-  imports: [RouterLink],
+  imports: [RouterLink, MatCardModule],
   templateUrl: './error-404.component.html',
   styleUrl: './error-404.component.scss'
 })
