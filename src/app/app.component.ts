0feb74adea36ede949f369fee2f951086080a6d2
Erona <eronameckaj1@gmail.com>
2025-01-05 11:30:54 +0100

    added spinner handling for route navigation events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,56 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import {
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { NgxSpinnerComponent, NgxSpinnerService } from 'ngx-spinner';
+import {
+  filter,
+  tap,
+  switchMap,
+  EMPTY,
+  takeUntil,
+  Observable,
+  timer,
+  throwError,
+  Subject,
+  map,
+  catchError,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslateModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, TranslateModule, NgxSpinnerComponent],
+  template: `
+    <router-outlet />
+    <ngx-spinner
+      bdColor="rgba(51,51,51,0.8)"
+      size="medium"
+      color="#fff"
+      type="ball-spin-clockwise"
+      [fullScreen]="true"
+      ><p style="color: white">Loading...</p></ngx-spinner
+    >
+  `,
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  readonly #router = inject(Router);
   readonly #translateService = inject(TranslateService);
+  readonly #spinnerService = inject(NgxSpinnerService);
+  readonly #unSub = new Subject<void>();
 
   ngOnInit(): void {
+    this.listenToRouteEvents();
     this.setUpLanguage();
+  }
+
+  ngOnDestroy(): void {
+    this.#unSub.next();
+    this.#unSub.complete();
   }
 
   private setUpLanguage(): void {
@@ -24,5 +62,59 @@ export class AppComponent implements OnInit {
     localStorage.setItem('language', selectedLang);
     this.#translateService.setDefaultLang(selectedLang);
     this.#translateService.use(selectedLang);
+  }
+
+  private listenToRouteEvents(): void {
+    this.#router.events
+      .pipe(
+        filter(
+          (event) =>
+            event instanceof NavigationStart ||
+            event instanceof NavigationEnd ||
+            event instanceof NavigationError
+        ),
+        tap((event) => this.handleSpinner(event)),
+        switchMap((event) =>
+          event instanceof NavigationEnd
+            ? this.handleNavigationEnd()
+            : event instanceof NavigationError
+            ? this.handleNavigationError(event)
+            : EMPTY
+        ),
+        catchError((error) => {
+          console.error('Global Navigation Error:', error);
+          return EMPTY;
+        }),
+        takeUntil(this.#unSub)
+      )
+      .subscribe();
+  }
+
+  private handleSpinner(event: any): void {
+    if (event instanceof NavigationStart) {
+      this.#spinnerService.show();
+    }
+  }
+
+  private handleNavigationEnd(): Observable<void> {
+    return timer(600).pipe(
+      tap(() => this.#spinnerService.hide()),
+      map(() => undefined)
+    );
+  }
+
+  private handleNavigationError(event: NavigationError): Observable<void> {
+    if (event.error instanceof Error) {
+      console.error('Navigation error occurred:', event.error);
+      if (event.error.name === 'ChunkLoadError') {
+        console.warn('ChunkLoadError occurred. Reloading...');
+        window.location.reload();
+        return EMPTY;
+      } else {
+        return throwError(() => new Error('Unhandled navigation error'));
+      }
+    }
+    console.error('Unknown navigation error:', event.error);
+    return throwError(() => new Error('Unknown navigation error'));
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  HttpClient,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { httpInterceptor } from './core/interceptors/http.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 export function HttpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
@@ -18,12 +27,15 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    importProvidersFrom(
+      NgxSpinnerModule.forRoot({ type: 'ball-spin-clockwise' })
+    ),
     provideTranslateService({
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
         deps: [HttpClient],
       },
-    })
-  ]
+    }),
+  ],
 };
